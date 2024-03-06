20c78f611dccea533cba8d013e80e054f114d400
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-06 23:06:04 +0500

    first
    Step 1

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Насрулло Исмоилжонов on 04/03/24.
+//
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    
+    var body: some View {
+        MapReader{ proxy in
+            Map(initialPosition: startPosition)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local){
+                        print("Tapped at \(coordinate)")
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
